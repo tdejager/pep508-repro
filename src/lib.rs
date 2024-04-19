6034d933efee0af71cd320017b00b086688717a2
Tim de Jager <tim@prefix.dev>
2024-04-19 16:16:05 +0200

    test: test it on ci

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    use pep508_rs::{Requirement, VersionOrUrl};
+    use std::str::FromStr;
+
+    #[test]
+    fn test_spec_str() {
+        let spec_str = "mypkg @ file:///C:/path/to/my_pkg";
+        let spec = Requirement::from_str(spec_str).unwrap();
+        let raw = uv_fs::normalize_url_path(pep508_rs::strip_host("///C:/path/to/my_pkg"));
+        if let Some(VersionOrUrl::Url(url)) = spec.version_or_url {
+            let path = url.path();
+            assert_eq!(path, raw);
+            println!("{path} == {raw}");
+        } else {
+            assert!(false, "expected url. Found: {spec_str}");
+        }
+    }
+}
