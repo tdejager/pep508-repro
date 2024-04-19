357b865c76ae980ba09712dce9ee0f93911b49bc
Tim de Jager <tim@prefix.dev>
2024-04-19 16:38:55 +0200

    feat: make more complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use pep508_rs::{Requirement, VersionOrUrl};
+    use pep508_rs::{split_scheme, Requirement, VersionOrUrl};
     use std::str::FromStr;
 
     #[test]
     fn test_spec_str() {
         let spec_str = "mypkg @ file:///C:/path/to/my_pkg";
         let spec = Requirement::from_str(spec_str).unwrap();
-        let raw = uv_fs::normalize_url_path(pep508_rs::strip_host("///C:/path/to/my_pkg"));
+        let raw = uv_fs::normalize_url_path(pep508_rs::strip_host(
+            split_scheme("file:///C:/path/to/my_pkg").unwrap().1,
+        ));
         if let Some(VersionOrUrl::Url(url)) = spec.version_or_url {
             let path = url.path();
             assert_eq!(path, raw);
